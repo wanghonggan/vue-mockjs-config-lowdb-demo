ea7f2813079f3ae24faeeb246548f7fe02becefe
wanghg <wanghg95@163.com>
2022-09-13 23:41:48 +0800

    22222

--- src/mock/service/user.js
+++ src/mock/service/user.js
@@ -5,8 +5,7 @@ const path = require('path');
 const mockjs = require('mockjs');
 const lowdb = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
-console.log(path.resolve(__dirname, 'user.json'))
-const db = lowdb(new FileSync(path.resolve(__dirname, 'user.json')));
+const db = lowdb(new FileSync(path.resolve(__dirname, '../lowdb/json/user.json')));
 const _ = db._;
 
 db.defaults(
