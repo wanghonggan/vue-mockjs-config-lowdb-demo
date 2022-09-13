e4680af0e11936fb7f5926f55ae2d96cad4e3a28
wanghg <wanghg95@163.com>
2022-09-13 22:25:45 +0800

    999

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,9 @@ module.exports = {
   devServer: {
     before(app) {
       // 判断是否为开发环境
-      if (process.env.NODE_ENV.toUpperCase() === 'DEVELOPMENT'||true) {
-        // 如果需要获取到 post 请求参数，把下面这行代码的注释解开
-        app.use(require('body-parser').json()); // 把 post 请求参数解析为 json 格式
-        app.use(require('@andremao/mockjs-config')); // 挂上中间件
-      }
+      // 如果需要获取到 post 请求参数，把下面这行代码的注释解开
+      app.use(require('body-parser').json()); // 把 post 请求参数解析为 json 格式
+      app.use(require('@andremao/mockjs-config')); // 挂上中间件
     },
   },
 };
